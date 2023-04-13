e7a075e10f03fe80f9f5b9bb3b8dea6b24e3e76f
AlexisAubreyBrusola <99746311+AlexisAubreyBrusola@users.noreply.github.com>
2023-04-13 14:10:35 +0800

    Modified so that the validation error messages in adding admin is shown

--- controllers/add-admin.controller.php
+++ controllers/add-admin.controller.php
@@ -1,6 +1,7 @@
 <?php
 class AddAdminController {
     private $model;
+    public $message;
     public function __construct($model) {
         $this->model = $model;
     }
@@ -8,20 +9,33 @@ class AddAdminController {
     public function addAdminController($firstname, $lastname, $email, $password, $confirm_password) {
         // Validate inputs
         if(empty($firstname) || empty($lastname) || empty($email) || empty($password) || empty($confirm_password)) {
-            return false;
+            $message = "Please fill-up all the fields. Failed to add account!";
+            return array(false, $message);
         } elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            return false;
+            $message = "Please use a valid email. Failed to add account!";
+            return array(false, $message);
         } elseif(strlen($password) < 8) {
-            return false;
+            $message = "Password must be atleast 8 characters long. Failed to add account!";
+            return array(false, $message);
         } elseif($password !== $confirm_password) {
-            return false;
+            $message = "Password must match. Failed to add account! ";
+            return array(false, $message);
         } else {
+            // Check if email already exists
+            $admin = $this->model->getAdminByEmail($email);
+            if($admin) {
+                $message = "Email already exists. Failed to add account!";
+                return array(false, $message);
+            }
+
             // Add admin
             $result = $this->model->addAdmin($firstname, $lastname, $email, $password);
             if($result) {
-                return true;
+                $message = "Admin Account added successfully";
+                return array(true, $message);
             } else {
-                return false;
+                $message = "There must be an error in the system, please try again.";
+                return array(false, $message);
             }
         }
     }
