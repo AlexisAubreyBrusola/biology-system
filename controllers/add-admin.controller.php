ee064ceb8102660209e0721d3609e88248e597bd
AlexisAubreyBrusola <99746311+AlexisAubreyBrusola@users.noreply.github.com>
2023-04-18 15:25:06 +0800

    Modify the returned values

--- controllers/add-admin.controller.php
+++ controllers/add-admin.controller.php
@@ -8,34 +8,32 @@ class AddAdminController {
     
     public function addAdminController($firstname, $lastname, $email, $password, $confirm_password) {
         // Validate inputs
-        if(empty($firstname) || empty($lastname) || empty($email) || empty($password) || empty($confirm_password)) {
-            $message = "Please fill-up all the fields. Failed to add account!";
-            return array(false, $message);
+        // Validate inputs
+        if(empty($firstname) || empty($lastname) || empty($email) || empty($password) || empty($confirm_password) || empty($contact_no)) {
+            return [false, "Please fill-up all the fields. Failed to add account!"];
+
         } elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $message = "Please use a valid email. Failed to add account!";
-            return array(false, $message);
+            return [false, "Please use a valid email. Failed to add account!"];
+
         } elseif(strlen($password) < 8) {
-            $message = "Password must be atleast 8 characters long. Failed to add account!";
-            return array(false, $message);
+            return [false, "Password must be atleast 8 characters long. Failed to add account!"];
+
         } elseif($password !== $confirm_password) {
-            $message = "Password must match. Failed to add account! ";
-            return array(false, $message);
+            return [false, "Password must match. Failed to add account!"];
         } else {
             // Check if email already exists
             $admin = $this->model->getAdminByEmail($email);
             if($admin) {
-                $message = "Email already exists. Failed to add account!";
-                return array(false, $message);
-            }
-
-            // Add admin
-            $result = $this->model->addAdmin($firstname, $lastname, $email, $password);
-            if($result) {
-                $message = "Admin Account added successfully";
-                return array(true, $message);
+                return [false, "Email already exists. Failed to add account!"];
             } else {
-                $message = "There must be an error in the system, please try again.";
-                return array(false, $message);
+                // Add admin
+                $result = $this->model->addAdmin($firstname, $lastname, $email, $password);
+                if($result) {
+                    return [true, "Admin Account added successfully"];
+                } else {
+                    $message = "There must be an error in the system, please try again.";
+                    return [false, "There must be an error in the system, please try again."];
+                }
             }
         }
     }
