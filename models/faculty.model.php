44ff658612b09c6406e350c7010c233edb2d95d8
AlexisAubreyBrusola <99746311+AlexisAubreyBrusola@users.noreply.github.com>
2023-04-20 09:43:21 +0800

    Modified the faculty.model.php

--- models/faculty.model.php
+++ models/faculty.model.php
@@ -1,23 +1,22 @@
 <?php
-class Student {
+class Faculty {
     private $db;
 
     public function __construct($db) {
         $this->db = $db;
     }
-    public function addFacultyInfo ($borrowerID, $facultyID, $department) {
-        $sql = "INSERT INTO student (borrower_id, faculty_id, department) VALUE (:borrower_id, :student_id, :course, :year_block)";
+    public function addFacultyInfo ($borrowerID, $faculty_id, $department) {
+        $sql = "INSERT INTO faculty (borrower_id, faculty_id, department) VALUE (:borrower_id, :faculty_id, :department)";
         $this->db->query($sql);
         $this->db->bind(':borrower_id', $borrowerID);
-        $this->db->bind(':student_id', $studentID);
-        $this->db->bind(':course', $course);
-        $this->db->bind(':year_block', $yearBlock);
+        $this->db->bind(':faculty_id', $faculty_id);
+        $this->db->bind(':department', $department);
 
         try {
             if($this->db->execute()) {
                 return true;
             } else {
-                throw new Exception("Failed to add student information.");
+                throw new Exception("Failed to add faculty's information.");
             }
         } catch (PDOException $e) {
             throw new Exception("Database error: " . $e->getMessage());
